fbeee01147b5f439fd1dee8e85dcdabd3ea3e2a6
alpacafluff <alpacafluff@users.noreply.github.com>
2018-04-16 14:58:41 -0400

    Create button-test.js

---
+++ button-test.js
@@ -0,0 +1,26 @@
+## Defining variables
+
+//var myVar = D1;
+//pinMode(myVar, 'input');
+
+### Button 
+var button = D1;
+pinMode(button, 'input-pulldown');
+
+#### Button is pressed test
+var button = D2;
+pinMode(button,'input_pulldown');
+
+setWatch(function(e) {
+  console.log("Button pressed");
+ toggle();
+}, button, { repeat: true, edge: 'rising' });
+
+
+  var on = false;
+  function toggle() {
+  on = !on;
+  digitalWrite(LED3, on);
+  //analogWrite(BUZZER, 0.5, { freq: 1000 } );
+  
+}
